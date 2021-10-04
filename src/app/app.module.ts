fd080979b9e2f6eaa38ba1ea79c48b86c6e1fbf6
Amphakarn <pisuthigomol@yahoo.com>
2021-10-04 16:13:13 +0000

    Setup Angular routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,6 +11,17 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  // Each route in this array is a JavaScript object that contains two properties.
+  // The first property, path, defines the URL path for the route.
+  // The second property, component, defines the component Angular should use for the corresponding path.
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent }
+
+]
 
 @NgModule({
   declarations: [
@@ -18,13 +30,16 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }) // {enableTracing: true}  helps with debugging routes. In order for the route you're on for that component to show, add <router-outlet> to app.component.html
   ],
   providers: [],
   bootstrap: [AppComponent]
